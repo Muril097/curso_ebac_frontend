9d2e6681804cd19a74b8b091006ba462b5c91e5a
Murilo Braga <murilobraga3@gmail.com>
2024-06-26 16:14:39 -0300

    correção da tarefa de jquerry

--- scritp.js
+++ scritp.js
@@ -10,23 +10,39 @@ $(document).ready(function(){
 
     $('form').on('submit', function(e){
         e.preventDefault();
-        check();
+
         const tarefa = $('#tarefa').val();
         const hora = $('#data-tarefa').val();
+
         const novoItem = $('<li></li>');
-        $(`<p>${tarefa}</p>`).appendTo(novoItem);
-        const novoTempo = $('<li></li>');
-        $(`<p>${hora}</p>`).appendTo(novoItem);
-        $(novoItem).appendTo('ul');
-        $('#tarefa').val('')
+        $(`<p>${tarefa} -- ${hora}</p>`).appendTo(novoItem) //até aqui está certo
+
+        /* const novoTempo = $('<li></li>');
+        $(`<p>${hora}</p>`).appendTo(novoItem);  essa parte não tinha nescessidade, dava para vincular a hora no item criado ascima
+
+        $(novoItem).appendTo('ul'); errado */ 
+        $("#lista-tarefas").append(novoItem); //correto
+
+            $('#tarefa').val('')
+            $('#data-tarefa').val('')
+
+            check();
     })
 
-    $('p').click(function(){
-        $('li').addClass('check');
-    })
+    function check(){
+        $('li')
+            .off('click')
+            .on('click', function() {
+                $(this).toggleClass("check");
+            })
+    }
+
+    //$('p').click(function(){
+        //$('li').addClass('check'); errado
+    //})
 })
-function check() {
+/*function check() {
     $("li").on("click", (e) => {
-    $(e.target).addClass("check");
+    $(e.target).addClass("check"); erradp
     });
-}
+}*/
